1dd7555fce62c0929f2322fa9048f098a9af5592
Constantin Heidegger (ETHZ) [cheidegg] <cheidegg@t3ui07.psi.ch>
2014-03-03 15:55:44 +0100

    added a not-yet-good way of producing the fake rate in the python macro

--- macros/frPlots.py
+++ macros/frPlots.py
@@ -1,6 +1,7 @@
 import ROOT, helper
 
 ROOT.gROOT.SetBatch(1)
+ROOT.gStyle.SetOptStat(0)
 
 def getColor(self):
 	mycolor = ROOT.TColor()
@@ -23,10 +24,10 @@ class sample:
 	color  = getColor
 	
 
-data   = sample('data'         , '../outputDir/data_ratios.root')
-wjets  = sample('wjets'        , '../outputDir/wjets_ratios.root')
-dyjets = sample('dyjets'       , '../outputDir/dyjets_ratios.root')
-qcd    = sample('qcdMuEnriched', '../outputDir/qcdMuEnriched_ratios.root')
+data   = sample('data'         , '../fakeHistos/data_ratios.root')
+wjets  = sample('wjets'        , '../fakeHistos/wjets_ratios.root')
+dyjets = sample('dyjets'       , '../fakeHistos/dyjets_ratios.root')
+qcd    = sample('qcdMuEnriched', '../fakeHistos/qcdMuEnriched_ratios.root')
 
 mc_samples = []
 mc_samples.append(qcd   )
@@ -44,17 +45,43 @@ leg.AddEntry(wjets .hists[0], 'W+Jets'  , 'f' )
 leg.AddEntry(dyjets.hists[0], 'DY+Jets' , 'f' )
 leg.AddEntry(qcd   .hists[0], 'QCD'     , 'f' )
 
-plotHists = ['h_Loose_muAwayJetDR', 'h_Loose_muAwayJetPt', 'h_Loose_muClosJetDR', 'h_Loose_muClosJetPt', 'h_Loose_muHT', 'h_Loose_muLepEta', 'h_Loose_muLepIso', 'h_Loose_muLepPt', 'h_Loose_muMET', 'h_Loose_muMETnoMTCut', 'h_Loose_muMT', 'h_Loose_muMTMET30', 'h_Loose_muMaxJPt', 'h_Loose_muNBJets', 'h_Loose_muNJets', 'h_Loose_muNVertices', 'h_Loose_muD0', 'h_Loose_muF', 'h_Tight_muAwayJetDR', 'h_Tight_muAwayJetPt', 'h_Tight_muClosJetDR', 'h_Tight_muClosJetPt', 'h_Tight_muHT', 'h_Tight_muLepEta', 'h_Tight_muLepIso', 'h_Tight_muLepPt', 'h_Tight_muMET', 'h_Tight_muMETnoMTCut', 'h_Tight_muMT', 'h_Tight_muMTMET30', 'h_Tight_muMaxJPt', 'h_Tight_muNBJets', 'h_Tight_muNJets', 'h_Tight_muNVertices', 'h_Tight_muD0', 'h_Tight_muF', 'h_muFakeRatio']
+plotHists = ['h_Loose_muAwayJetDR', 'h_Loose_muAwayJetPt', 'h_Loose_muClosJetDR', 'h_Loose_muClosJetPt', 'h_Loose_muHT', 'h_Loose_muLepEta', 'h_Loose_muLepIso', 'h_Loose_muLepPt', 'h_Loose_muMET', 'h_Loose_muMETnoMTCut', 'h_Loose_muMT', 'h_Loose_muMTMET30', 'h_Loose_muMaxJPt', 'h_Loose_muNBJets', 'h_Loose_muNJets', 'h_Loose_muNVertices', 'h_Loose_muD0', 'h_Tight_muAwayJetDR', 'h_Tight_muAwayJetPt', 'h_Tight_muClosJetDR', 'h_Tight_muClosJetPt', 'h_Tight_muHT', 'h_Tight_muLepEta', 'h_Tight_muLepIso', 'h_Tight_muLepPt', 'h_Tight_muMET', 'h_Tight_muMETnoMTCut', 'h_Tight_muMT', 'h_Tight_muMTMET30', 'h_Tight_muMaxJPt', 'h_Tight_muNBJets', 'h_Tight_muNJets', 'h_Tight_muNVertices', 'h_Tight_muD0']
 
-plotSingle = ['h_Loose_muF', 'h_Tight_muF']
+plotSingle = []
+
+
+for hist in qcd.hists:
+	if hist.GetName() == 'h_muFLoose':
+		FR_qcd_den = hist.ProjectionX()
+		FR_qcd_den.SetName("FR_qcd_den")
+	if hist.GetName() == 'h_muFTight':
+		FR_qcd = hist.ProjectionX()
+		FR_qcd.SetName("FR_qcd")
+
 
 for hist in data.hists:
 
 	i = data.hists.index(hist)
-	if hist.GetName() == 'h_Loose_muLepPt':
-		FR_den = hist
-	if hist.GetName() == 'h_Tight_muLepPt':
-		FR_num = hist
+	if hist.GetName() == 'h_muFLoose':
+		FR_data_den = hist.ProjectionX()
+		FR_data_den.SetName("FR_data_den")
+		FR_bg_den = FR_data_den.Clone()
+		FR_bg_den.SetName("FR_bg_den")
+		FR_bg_den.Reset()
+
+		for mc in mc_samples:
+			FR_bg_den.Add(mc.hists[i].ProjectionX())
+		
+	if hist.GetName() == 'h_muFTight':
+		FR_data = hist.ProjectionX()
+		FR_data.SetName("FR_data")
+		FR_bg = FR_data.Clone()
+		FR_bg.SetName("FR_bg")
+		FR_bg.Reset()
+
+		for mc in mc_samples:
+			FR_bg.Add(mc.hists[i].ProjectionX())
+
 	if not hist.GetName() in plotHists: continue
 
 	prepend = ''
@@ -90,6 +117,24 @@ for hist in data.hists:
 
 
 
+FR_data.Divide(FR_data_den)
+FR_data.SetFillColor(ROOT.kBlack)
+FR_bg.Divide(FR_bg_den)
+FR_bg.SetFillColor(ROOT.kRed)
+FR_qcd.Divide(FR_qcd_den)
+FR_qcd.SetFillColor(ROOT.kBlue)
+
+FR_bg.Draw("hist")
+FR_qcd.Draw("hist same")
+FR_data.Draw("e same")
+
+leg = helper.makeLegend(0.15, 0.35, 0.4, 0.60)
+leg.AddEntry(FR_data, 'Data'  , 'pe')
+leg.AddEntry(FR_bg, 'QCD + EW', 'f' )
+leg.AddEntry(FR_qcd, 'QCD'    , 'f' )
+leg.Draw()
+
+helper.saveCanvas(canv, "muFakeRatio")
 
 
 
