99653d918ce83bd13610d89757627eb8a5d2b064
Constantin Heidegger (ETHZ) [cheidegg] <cheidegg@t3ui07.psi.ch>
2014-03-03 16:20:04 +0100

    a first working version of the FR plots

--- macros/frPlots.py
+++ macros/frPlots.py
@@ -47,8 +47,6 @@ leg.AddEntry(qcd   .hists[0], 'QCD'     , 'f' )
 
 plotHists = ['h_Loose_muAwayJetDR', 'h_Loose_muAwayJetPt', 'h_Loose_muClosJetDR', 'h_Loose_muClosJetPt', 'h_Loose_muHT', 'h_Loose_muLepEta', 'h_Loose_muLepIso', 'h_Loose_muLepPt', 'h_Loose_muMET', 'h_Loose_muMETnoMTCut', 'h_Loose_muMT', 'h_Loose_muMTMET30', 'h_Loose_muMaxJPt', 'h_Loose_muNBJets', 'h_Loose_muNJets', 'h_Loose_muNVertices', 'h_Loose_muD0', 'h_Tight_muAwayJetDR', 'h_Tight_muAwayJetPt', 'h_Tight_muClosJetDR', 'h_Tight_muClosJetPt', 'h_Tight_muHT', 'h_Tight_muLepEta', 'h_Tight_muLepIso', 'h_Tight_muLepPt', 'h_Tight_muMET', 'h_Tight_muMETnoMTCut', 'h_Tight_muMT', 'h_Tight_muMTMET30', 'h_Tight_muMaxJPt', 'h_Tight_muNBJets', 'h_Tight_muNJets', 'h_Tight_muNVertices', 'h_Tight_muD0']
 
-plotSingle = []
-
 
 for hist in qcd.hists:
 	if hist.GetName() == 'h_muFLoose':
@@ -88,32 +86,19 @@ for hist in data.hists:
 	postpend = ''
 	if '_Loose_' in hist.GetName(): prepend = 'Loose_'
 	if '_Tight_' in hist.GetName(): prepend = 'Tight_'
-	
-	if hist.GetName() in plotSingle:
-		hist.Draw("text colz e")
-		postpend = '_data'
-		helper.saveCanvas(canv, prepend + helper.getSaveName(hist) + postpend)
 
-		for mc in mc_samples:
-			mc.hists[i].Draw("text colz e")
-			postpend = '_' + mc.name
-			helper.saveCanvas(canv, prepend + helper.getSaveName(mc.hists[i]) + postpend)
-
-	else:
-		stack = ROOT.THStack()
-		stackint = 0.
-		for mc in mc_samples:
-			stackint += mc.hists[i].Integral()
-			stack.Add(mc.hists[i])
-		yscale = max(stack.GetMaximum(), hist.GetMaximum())
-		stack.Draw('hist')
-		stack.SetMaximum(1.2*yscale)
-		stack.GetXaxis().SetTitle(helper.getXTitle(hist))
-		## hist.Scale(stackint/hist.Integral())
-		if not hist.GetName() in plotSingle: # with this step, we plot only total mc (stack) without data as data already is plotted solely before
-			hist.Draw('p e1 same')
-			leg.Draw()
-		helper.saveCanvas(canv, prepend + helper.getSaveName(hist) + postpend)
+	stack = ROOT.THStack()
+	stackint = 0.
+	for mc in mc_samples:
+		stackint += mc.hists[i].Integral()
+		stack.Add(mc.hists[i])
+	yscale = max(stack.GetMaximum(), hist.GetMaximum())
+	stack.Draw('hist')
+	stack.SetMaximum(1.2*yscale)
+	stack.GetXaxis().SetTitle(helper.getXTitle(hist))
+	hist.Draw('p e1 same')
+	leg.Draw()
+	helper.saveCanvas(canv, prepend + helper.getSaveName(hist) + postpend)
 
 
 
@@ -128,11 +113,11 @@ FR_bg.Draw("hist")
 FR_qcd.Draw("hist same")
 FR_data.Draw("e same")
 
-leg = helper.makeLegend(0.15, 0.35, 0.4, 0.60)
-leg.AddEntry(FR_data, 'Data'  , 'pe')
-leg.AddEntry(FR_bg, 'QCD + EW', 'f' )
-leg.AddEntry(FR_qcd, 'QCD'    , 'f' )
-leg.Draw()
+legend = helper.makeLegend(0.15, 0.35, 0.4, 0.60)
+legend.AddEntry(FR_data, 'Data'  , 'pe')
+legend.AddEntry(FR_bg, 'QCD + EW', 'f' )
+legend.AddEntry(FR_qcd, 'QCD'    , 'f' )
+legend.Draw()
 
 helper.saveCanvas(canv, "muFakeRatio")
 
