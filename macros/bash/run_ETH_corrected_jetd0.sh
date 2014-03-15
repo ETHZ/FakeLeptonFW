98d1a2a3a425d92ac058665b5037c4b486058a2d
Constantin Heidegger (ETHZ) [cheidegg] <cheidegg@t3ui07.psi.ch>
2014-03-15 09:44:46 +0100

    nothing much

--- macros/bash/run_ETH_corrected_jetd0.sh
+++ macros/bash/run_ETH_corrected_jetd0.sh
@@ -2,8 +2,8 @@
 python frPlots.py ../fakeHistos/ETH_corrected_jetd0/ Plots/ETH_corrected_jetd0/unweighted/ none
 python frPlots.py ../fakeHistos/ETH_corrected_jetd0/ Plots/ETH_corrected_jetd0/fit_weighted/ fit
 python frPlots.py ../fakeHistos/ETH_corrected_jetd0/ Plots/ETH_corrected_jetd0/qcd_weighted/ qcd
-python frPlots.py ../fakeHistos/ETH_corrected_jetd0/ Plots/ETH_corrected_jetd0/qcdwjetd0s_weighted/ qcdwjetd0s
+python frPlots.py ../fakeHistos/ETH_corrected_jetd0/ Plots/ETH_corrected_jetd0/qcdwjets_weighted/ qcdwjets
 cp Plots/ETH_corrected_jetd0/unweighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_jetd0/unweighted
 cp Plots/ETH_corrected_jetd0/fit_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_jetd0/fit_weighted
 cp Plots/ETH_corrected_jetd0/qcd_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_jetd0/qcd_weighted
-cp Plots/ETH_corrected_jetd0/qcdwjetd0s_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_jetd0/qcdwjetd0s_weighted
+cp Plots/ETH_corrected_jetd0/qcdwjets_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_jetd0/qcdwjets_weighted
--- macros/bash/run_ETH_corrected_tot.sh
+++ macros/bash/run_ETH_corrected_tot.sh
@@ -2,8 +2,8 @@
 python frPlots.py ../fakeHistos/ETH_corrected_tot/ Plots/ETH_corrected_tot/unweighted/ none
 python frPlots.py ../fakeHistos/ETH_corrected_tot/ Plots/ETH_corrected_tot/fit_weighted/ fit
 python frPlots.py ../fakeHistos/ETH_corrected_tot/ Plots/ETH_corrected_tot/qcd_weighted/ qcd
-python frPlots.py ../fakeHistos/ETH_corrected_tot/ Plots/ETH_corrected_tot/qcdwtots_weighted/ qcdwtots
+python frPlots.py ../fakeHistos/ETH_corrected_tot/ Plots/ETH_corrected_tot/qcdwjets_weighted/ qcdwjets
 cp Plots/ETH_corrected_tot/unweighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_tot/unweighted
 cp Plots/ETH_corrected_tot/fit_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_tot/fit_weighted
 cp Plots/ETH_corrected_tot/qcd_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_tot/qcd_weighted
-cp Plots/ETH_corrected_tot/qcdwtots_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_tot/qcdwtots_weighted
+cp Plots/ETH_corrected_tot/qcdwjets_weighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/ETH_corrected_tot/qcdwjets_weighted
