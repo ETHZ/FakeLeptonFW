2b7d05799960b4e39e23519489cb1bb76e48e3df
Constantin Heidegger (ETHZ) [cheidegg] <cheidegg@t3ui07.psi.ch>
2014-04-30 12:32:53 +0200

    tidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying uptidying up

--- macros/runOldB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm Plots/runOldB/unweighted/*.png
-rm Plots/runOldB/unweighted/*.pdf
-python frPlots.py ../fakeHistos/runOldB/ Plots/runOldB/unweighted/ none
-rm /afs/cern.ch/user/c/cheidegg/www/pdfs/runOldB/unweighted/*.png
-rm /afs/cern.ch/user/c/cheidegg/www/pdfs/runOldB/unweighted/*.pdf
-cp Plots/runOldB/unweighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/runOldB/unweighted
--- macros/runTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python frPlots.py ../fakeHistos/PUweight_runD/ETH_corrected_jet_test/ Plots/PUweight_runD/ETH_corrected_jet_test/unweighted/ none
-cp Plots/PUweight_runD/ETH_corrected_jet_test/unweighted/*.p?? /afs/cern.ch/user/c/cheidegg/www/pdfs/PUweight_runD/ETH_corrected_jet_test/unweighted
