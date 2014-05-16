67322dbd3c2dadc090ceb82e32e34204c4f7f945
mdunser <marc.dunser@cern.ch>
2014-05-16 17:36:24 +0200

    error calculation and some sort of propagation also added helper.py agian, because i like the name

--- macros/closure.py
+++ macros/closure.py
@@ -1,4 +1,4 @@
-import ROOT
+import ROOT, math, helper
 
 
 #f = ROOT.TFile('../closureTest/ttjets_ttbarTruthFR_closureOutput.root','read')
@@ -10,32 +10,39 @@ tree = f.Get('closureTree')
 class cat:
 	def __init__(self, name):
 		self.name = name
-	ntt, ntl, nlt, nll     = 0., 0., 0., 0.
 	nttc, ntlc, nltc, nllc = 0 , 0 , 0 , 0
+	ntt, ntl, nlt, nll     = 0., 0., 0., 0.
 	npp, npf, nfp, nff     = 0 , 0 , 0 , 0
+	ntt2, ntl2, nlt2, nll2     = 0., 0., 0., 0.
+	npp2, npf2, nfp2, nff2     = 0 , 0 , 0 , 0
 
 
 sr = 2
 
-mm = cat('mu-mu')
-em = cat('el-mu')
-ee = cat('el-el')
+mm = cat('MUON-MUON')
+em = cat('ELECTRON-MUON')
+ee = cat('ELECTRON-ELECTRON')
 
 cats = [mm, em, ee] ## this order is very important!!!!
 
 for evt in tree:
 
+	weight = evt.lumiW
+
 	if sr == 1:
-		if evt.nj   <  2: continue
-		if evt.nb  !=  0: continue
+		if evt.pt1 < 20 or evt.pt2 < 20: continue
+		if evt.nj   <   2: continue
+		if evt.nb  !=   0: continue
 		if evt.met  < 30.: continue
 
 	if sr == 2:
+		if evt.pt1 < 20 or evt.pt2 < 20: continue
 		if evt.nj  <   2: continue
 		if evt.nb  <   1: continue
 		if evt.met < 30.: continue
 
 	if sr == 3:
+		if evt.pt1 < 20 or evt.pt2 < 20: continue
 		if evt.nj  <   2: continue
 		if evt.nb  <   2: continue
 		if evt.met < 30.: continue
@@ -43,52 +50,79 @@ for evt in tree:
 	if sr == 0:
 		pass
 
-	cats[evt.type].npp += evt.npp*evt.lumiW
-	cats[evt.type].npf += evt.npf*evt.lumiW
-	cats[evt.type].nfp += evt.nfp*evt.lumiW
-	cats[evt.type].nff += evt.nff*evt.lumiW
+	cats[evt.type].npp += evt.npp*weight
+	cats[evt.type].npf += evt.npf*weight
+	cats[evt.type].nfp += evt.nfp*weight
+	cats[evt.type].nff += evt.nff*weight
 
-	if evt.tlcat is 0: cats[evt.type].ntt +=1*evt.lumiW
-	if evt.tlcat is 1: cats[evt.type].nlt +=1*evt.lumiW
-	if evt.tlcat is 2: cats[evt.type].ntl +=1*evt.lumiW
-	if evt.tlcat is 3: cats[evt.type].nll +=1*evt.lumiW
+	cats[evt.type].npp2 += evt.npp*evt.npp*weight*weight
+	cats[evt.type].npf2 += evt.npf*evt.npf*weight*weight
+	cats[evt.type].nfp2 += evt.nfp*evt.nfp*weight*weight
+	cats[evt.type].nff2 += evt.nff*evt.nff*weight*weight
 
-	if evt.tlcat is 0: cats[evt.type].nttc +=1
-	if evt.tlcat is 1: cats[evt.type].nltc +=1
-	if evt.tlcat is 2: cats[evt.type].ntlc +=1
-	if evt.tlcat is 3: cats[evt.type].nllc +=1
+	if   evt.tlcat is 0:
+		cats[evt.type].ntt  +=weight
+		cats[evt.type].ntt2 +=weight*weight
+		cats[evt.type].nttc +=1
+
+	elif evt.tlcat is 1:
+		cats[evt.type].ntl  +=weight
+		cats[evt.type].ntl2 +=weight*weight
+		cats[evt.type].ntlc +=1
+
+	elif evt.tlcat is 2:
+		cats[evt.type].nlt  +=weight
+		cats[evt.type].nlt2 +=weight*weight
+		cats[evt.type].nltc +=1
+
+	elif evt.tlcat is 3:
+		cats[evt.type].nll  +=weight
+		cats[evt.type].nll2 +=weight*weight
+		cats[evt.type].nllc +=1
 
 
 
 for cat in cats:
 	cat.fakes = cat.npf+cat.nfp+cat.nff
 	cat.obs   = cat.ntt
+	cat.ntte , cat.ntle , cat.nlte , cat.nlle  = math.sqrt(cat.ntt2), math.sqrt(cat.ntl2), math.sqrt(cat.nlt2), math.sqrt(cat.nll2)
+	cat.nttce, cat.ntlce, cat.nltce, cat.nllce = math.sqrt(cat.nttc), math.sqrt(cat.ntlc), math.sqrt(cat.nltc), math.sqrt(cat.nllc)
+	cat.nppe , cat.npfe , cat.nfpe , cat.nffe  = math.sqrt(cat.npp2), math.sqrt(cat.npf2), math.sqrt(cat.nfp2), math.sqrt(cat.nff2)
+	cat.fakese = cat.npfe+cat.nfpe+cat.nffe
+	cat.obse  = cat.ntte
 	
-	print '\n\n\nCATEGORY:', cat.name
-	print '=================================================================='
-	print '=================================================================='
-	## print 'ntt: %.3f  ntl: %.3f  nlt: %.3f  nll: %.3f   sum: %.3f' %(cat.ntt, cat.ntl, cat.nlt, cat.nll, cat.ntt+cat.ntl+cat.nlt+cat.nll)
-	print '%10s \t| %10s \t| %10s \t| %10s \t|| %10s' %('NTT', 'NTL', 'NLT', 'NLL', 'SUM')
-	print '-------------------------------------------------------------------------------------'
-	print '%10.2f \t| %10.2f \t| %10.2f \t| %10.2f \t|| %10.2f' %(cat.ntt, cat.ntl, cat.nlt, cat.nll, cat.ntt+cat.ntl+cat.nlt+cat.nll)
+	print '\n\n\n'
+	print '=============================================================================================================='
+	print '                                          CATEGORY:', cat.name
+	print '=============================================================================================================='
+	print '%10s%9s | %10s%9s | %10s%9s | %10s%9s || %10s%9s' %('NTT','', 'NTL','', 'NLT','', 'NLL','', 'SUM','')
+	print '--------------------------------------------------------------------------------------------------------------'
+	print '%10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f || %10.2f +- %5.2f' %(
+	      cat.ntt, cat.ntte, cat.ntl, cat.ntle, cat.nlt, cat.nlte, cat.nll, cat.nlle, cat.ntt+cat.ntl+cat.nlt+cat.nll, cat.ntte+cat.ntle+cat.nlte+cat.nlle)
 
-	print '-------------------------------------------------------------------------------------'
-	print '-------------------------------------------------------------------------------------'
+	print '--------------------------------------------------------------------------------------------------------------'
+	print '--------------------------------------------------------------------------------------------------------------'
 
-	print '%10s \t| %10s \t| %10s \t| %10s \t|| %10s' %('NPP', 'NPF', 'NFP', 'NFF', 'SUM')
-	print '-------------------------------------------------------------------------------------'
-	print '%10.2f \t| %10.2f \t| %10.2f \t| %10.2f \t|| %10.2f' %(cat.npp, cat.npf, cat.nfp, cat.nff, cat.npp+cat.npf+cat.nfp+cat.nff)
+	print '%10s%9s | %10s%9s | %10s%9s | %10s%9s || %10s%9s' %('NPP','', 'NPF','', 'NFP','', 'NFF','', 'SUM','')
+	print '--------------------------------------------------------------------------------------------------------------'
+	print '%10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f || %10.2f +- %5.2f' %(
+	      cat.npp, cat.nppe, cat.npf, cat.npfe, cat.nfp, cat.nfpe, cat.nff, cat.nffe, cat.npp+cat.npf+cat.nfp+cat.nff, cat.nppe+cat.npfe+cat.nfpe+cat.nffe)
 
-	print '-------------------------------------------------------------------------------------'
-	print 'SUM OF FAKES: %.3f' %(cat.fakes)
-	print 'OBSERVED    : %.3f' %(cat.obs)
+	print '--------------------------------------------------------------------------------------------------------------'
+	print 'OBSERVED     : %.2f +- %.2f' %(cat.obs  , cat.ntte)
+	print 'SUM OF FAKES : %.2f +- %.2f' %(cat.fakes, cat.npfe+cat.nfpe+cat.nffe)
 
-	
-	print '\n %25s %.3f' %('pred./ obs.:', cat.fakes/cat.obs)
-	print '\n %25s %.3f' %('(pred. - obs.) / pred.:', (cat.fakes - cat.obs)/cat.fakes)
+	res    = helper.divWithErr(cat.fakes, cat.fakese, cat.obs, cat.obse)	
+	relres = helper.divWithErr(cat.fakes - cat.obs, cat.fakese - cat.obse, cat.fakes, cat.fakese)	
+	print '\n------------------------------------------'
+	print '%25s %.3f +- %.3f' %('pred./ obs.:', res[0], res[1])
+	print '\n%25s %.3f +- %.3f' %('(pred. - obs.) / pred.:', relres[0], relres[1])
+	print '------------------------------------------'
 	
 	print '\n \nPURE COUNTS:'
-	print '%10s \t| %10s \t| %10s \t| %10s \t|| %10s' %('NTT', 'NTL', 'NLT', 'NLL', 'SUM')
-	print '-------------------------------------------------------------------------------------'
-	print '%10.2f \t| %10.2f \t| %10.2f \t| %10.2f \t|| %10.2f' %(cat.nttc, cat.ntlc, cat.nltc, cat.nllc, cat.nttc+cat.ntlc+cat.nltc+cat.nllc)
+	print '%10s%9s | %10s%9s | %10s%9s | %10s%9s || %10s%9s' %('NTT','', 'NTL','', 'NLT','', 'NLL','', 'SUM','')
+	print '--------------------------------------------------------------------------------------------------------------'
+	print '%10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f | %10.2f +- %5.2f || %10.2f +- %5.2f' %(
+	      cat.nttc, cat.nttce, cat.ntlc, cat.ntlce, cat.nltc, cat.nltce, cat.nllc, cat.nllce, cat.nttc+cat.ntlc+cat.nltc+cat.nllc, cat.nttce+cat.ntlce+cat.nltce+cat.nllce)
 
+f.Close()
