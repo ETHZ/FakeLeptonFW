37305b308e2816829bcdfe27095ea8bcef4325f9
Constantin Heidegger (ETHZ) [cheidegg] <cheidegg@t3ui07.psi.ch>
2014-05-16 15:22:16 +0200

    nothing much

--- macros/bash/ETH/corrected_jet/PUweight_runD/Ele17JMC/0.sh
+++ macros/bash/ETH/corrected_jet/PUweight_runD/Ele17JMC/0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ unweighted/
-runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ fit_weighted/
-runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ qcd_weighted/
-runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ qcdwjets_weighted/
+#runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ fit_weighted/
+#runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ qcd_weighted/
+#runFR el ETH/corrected_jet/PUweight_runD/Ele17JMC/ qcdwjets_weighted/
