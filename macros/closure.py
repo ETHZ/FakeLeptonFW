d8f40c58a99afa19264de8aa1efc2cb362a0f5de
mdunser <marc.dunser@cern.ch>
2014-06-05 12:57:01 +0200

    commiting my latest code

--- macros/closure.py
+++ macros/closure.py
@@ -1,7 +1,7 @@
 import ROOT, math, helper
 
 
-f = ROOT.TFile('../closureTest/ttjets_semiLep_closureOutput.root','read')
+#f = ROOT.TFile('../closureTest/ttjets_semiLep_closureOutput.root','read')
 #f = ROOT.TFile('../closureTest/ttjets_fullLep_closureOutput.root','read')
 #f = ROOT.TFile('../closureTest/ttjets_massiveb_newEl_closureOutput.root','read')
 #f = ROOT.TFile('../closureTest/ttjets_semilep_elchcons_closureOutput.root','read')
@@ -10,98 +10,91 @@ f = ROOT.TFile('../closureTest/ttjets_semiLep_closureOutput.root','read')
 #f = ROOT.TFile('../closureTest/wjets_inc_trigger_closureOutput.root','read')
 #f = ROOT.TFile('../closureTest/ttjets_massiveb_elchcons_closureOutput.root','read')
 
-tree = f.Get('closureTree')
-
-class cat:
-	def __init__(self, name):
-		self.name = name
-	nttc, ntlc, nltc, nllc = 0 , 0 , 0 , 0
-	ntt, ntl, nlt, nll     = 0., 0., 0., 0.
-	npp, npf, nfp, nff     = 0 , 0 , 0 , 0
-	ntt2, ntl2, nlt2, nll2     = 0., 0., 0., 0.
-	npp2, npf2, nfp2, nff2     = 0 , 0 , 0 , 0
+ttjets = helper.sample('ttjets','../closureTest/ttjets_semiLep_closureOutput.root')
 
 
 sr = 2
 
-mm = cat('MUON-MUON')
-em = cat('ELECTRON-MUON')
-ee = cat('ELECTRON-ELECTRON')
+## mm = helper.cat('MUON-MUON')
+## em = helper.cat('ELECTRON-MUON')
+## ee = helper.cat('ELECTRON-ELECTRON')
 
-cats = [mm, em, ee] ## this order is very important!!!!
+##cats = [mm, em, ee] ## this order is very important!!!!
+samples = [ttjets]
 
 trigger = 1
 
-for evt in tree:
+for sample in samples:
+	for evt in sample.tree:
+	
+		if trigger and evt.passTrigger == 0: continue
+		weight = evt.lumiW
+		type   = evt.type
+		if type > 2:
+			type -= 3
+	
+		if sr == 1:
+			if evt.pt1 < 20 or evt.pt2 < 20: continue
+			if evt.nj   <   2: continue
+			if evt.nb  !=   0: continue
+			if evt.met  < 30.: continue
+	
+		if sr == 2:
+			if evt.type > 2: continue
+			if evt.pt1 < 20 or evt.pt2 < 20: continue
+			if evt.nj  <   2: continue
+			if evt.nb  <   1: continue
+			if evt.met < 30.: continue
+	
+		if sr == 3:
+			if evt.pt1 < 20 or evt.pt2 < 20: continue
+			if evt.nj  <   2: continue
+			if evt.nb  <   2: continue
+			if evt.met < 30.: continue
+	
+		if sr == 0:
+			pass
+	
+		sample.cats[type].npp += evt.npp*weight
+		sample.cats[type].npf += evt.npf*weight
+		sample.cats[type].nfp += evt.nfp*weight
+		sample.cats[type].nff += evt.nff*weight
+	
+		sample.cats[type].npp2 += evt.npp*evt.npp*weight*weight
+		sample.cats[type].npf2 += evt.npf*evt.npf*weight*weight
+		sample.cats[type].nfp2 += evt.nfp*evt.nfp*weight*weight
+		sample.cats[type].nff2 += evt.nff*evt.nff*weight*weight
+	
+		if   evt.tlcat is 0:
+			sample.cats[type].ntt  +=weight
+			sample.cats[type].ntt2 +=weight*weight
+			sample.cats[type].nttc +=1
+	
+		elif evt.tlcat is 1:
+			sample.cats[type].ntl  +=weight
+			sample.cats[type].ntl2 +=weight*weight
+			sample.cats[type].ntlc +=1
+	
+		elif evt.tlcat is 2:
+			sample.cats[type].nlt  +=weight
+			sample.cats[type].nlt2 +=weight*weight
+			sample.cats[type].nltc +=1
+	
+		elif evt.tlcat is 3:
+			sample.cats[type].nll  +=weight
+			sample.cats[type].nll2 +=weight*weight
+			sample.cats[type].nllc +=1
 
-	if trigger and evt.passTrigger == 0: continue
-	weight = evt.lumiW
-	type   = evt.type
-	if type > 2:
-		type -= 3
 
-	if sr == 1:
-		if evt.pt1 < 20 or evt.pt2 < 20: continue
-		if evt.nj   <   2: continue
-		if evt.nb  !=   0: continue
-		if evt.met  < 30.: continue
-
-	if sr == 2:
-		if evt.type > 2: continue
-		if evt.pt1 < 20 or evt.pt2 < 20: continue
-		if evt.nj  <   2: continue
-		if evt.nb  <   1: continue
-		if evt.met < 30.: continue
-
-	if sr == 3:
-		if evt.pt1 < 20 or evt.pt2 < 20: continue
-		if evt.nj  <   2: continue
-		if evt.nb  <   2: continue
-		if evt.met < 30.: continue
-
-	if sr == 0:
-		pass
-
-	cats[type].npp += evt.npp*weight
-	cats[type].npf += evt.npf*weight
-	cats[type].nfp += evt.nfp*weight
-	cats[type].nff += evt.nff*weight
-
-	cats[type].npp2 += evt.npp*evt.npp*weight*weight
-	cats[type].npf2 += evt.npf*evt.npf*weight*weight
-	cats[type].nfp2 += evt.nfp*evt.nfp*weight*weight
-	cats[type].nff2 += evt.nff*evt.nff*weight*weight
-
-	if   evt.tlcat is 0:
-		cats[type].ntt  +=weight
-		cats[type].ntt2 +=weight*weight
-		cats[type].nttc +=1
-
-	elif evt.tlcat is 1:
-		cats[type].ntl  +=weight
-		cats[type].ntl2 +=weight*weight
-		cats[type].ntlc +=1
-
-	elif evt.tlcat is 2:
-		cats[type].nlt  +=weight
-		cats[type].nlt2 +=weight*weight
-		cats[type].nltc +=1
-
-	elif evt.tlcat is 3:
-		cats[type].nll  +=weight
-		cats[type].nll2 +=weight*weight
-		cats[type].nllc +=1
-
-
-
-for cat in cats:
-	cat.fakes = cat.npf+cat.nfp+cat.nff
-	cat.obs   = cat.ntt
-	cat.ntte , cat.ntle , cat.nlte , cat.nlle  = math.sqrt(cat.ntt2), math.sqrt(cat.ntl2), math.sqrt(cat.nlt2), math.sqrt(cat.nll2)
-	cat.nttce, cat.ntlce, cat.nltce, cat.nllce = math.sqrt(cat.nttc), math.sqrt(cat.ntlc), math.sqrt(cat.nltc), math.sqrt(cat.nllc)
-	cat.nppe , cat.npfe , cat.nfpe , cat.nffe  = math.sqrt(cat.npp2), math.sqrt(cat.npf2), math.sqrt(cat.nfp2), math.sqrt(cat.nff2)
-	cat.fakese = cat.npfe+cat.nfpe+cat.nffe
-	cat.obse  = cat.ntte
+for sample in samples:
+	for cat in sample.cats:
+		sample.cat.fakes = sample.cat.npf+sample.cat.nfp+sample.cat.nff
+		sample.cat.obs   = sample.cat.ntt
+		sample.cat.ntte , sample.cat.ntle , sample.cat.nlte , sample.cat.nlle  = math.sqrt(sample.cat.ntt2), math.sqrt(sample.cat.ntl2), math.sqrt(sample.cat.nlt2), math.sqrt(sample.cat.nll2)
+		sample.cat.nttce, sample.cat.ntlce, sample.cat.nltce, sample.cat.nllce = math.sqrt(sample.cat.nttc), math.sqrt(sample.cat.ntlc), math.sqrt(sample.cat.nltc), math.sqrt(sample.cat.nllc)
+		sample.cat.nppe , sample.cat.npfe , sample.cat.nfpe , sample.cat.nffe  = math.sqrt(sample.cat.npp2), math.sqrt(sample.cat.npf2), math.sqrt(sample.cat.nfp2), math.sqrt(sample.cat.nff2)
+		sample.cat.fakese = sample.cat.npfe+sample.cat.nfpe+sample.cat.nffe
+		sample.cat.obse  = sample.cat.ntte
 	
 	print '\n\n\n'
 	print '=============================================================================================================='
--- macros/helper.py
+++ macros/helper.py
@@ -7,3 +7,22 @@ def divWithErr(a, ae, b, be):
 	relerr_b = be/b
 	relerr_tot = relerr_a + relerr_b
 	return central, central*relerr_tot
+
+class cat:
+	def __init__(self, name):
+		self.name = name
+	nttc, ntlc, nltc, nllc = 0 , 0 , 0 , 0
+	ntt, ntl, nlt, nll     = 0., 0., 0., 0.
+	npp, npf, nfp, nff     = 0 , 0 , 0 , 0
+	ntt2, ntl2, nlt2, nll2     = 0., 0., 0., 0.
+	npp2, npf2, nfp2, nff2     = 0 , 0 , 0 , 0
+
+class sample:
+	def __init__(self, name, file):
+		self.name = name
+		self.file = file
+		self.tree = ROOT.TFile(file).Get('closureTree')
+	mm = cat('MUON-MUON')
+	em = cat('MUON-ELECTRON')
+	ee = cat('ELECTRON-ELECTRON')
+	cats = [mm, em, ee]
